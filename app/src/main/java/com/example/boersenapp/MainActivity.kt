797247a07033039a673b40d3926a7271173c242e
HannesWeizmann <hannesweizmann54@gmail.com>
2023-02-08 09:18:22 +0100

    Kleine Fehlerbehebungen

--- app/src/main/java/com/example/boersenapp/MainActivity.kt
+++ app/src/main/java/com/example/boersenapp/MainActivity.kt
@@ -13,10 +13,6 @@ import com.example.boersenapp.api.tickers.dataclass.Tickers
 import com.example.boersenapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.create
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-       //getDataTickers()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -44,36 +38,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-/*
-    private fun getDataTickers(){
-
-        val tickerAPi = RetrofitHelper.getInstance().create(TickersAPI::class.java)
-        val accesskey ="a1626f7c013a2fbc9d4431d2fb8b68b1"
-        val limit = 2
-        val call: Call<Tickers> = tickerAPi.getTickers(accesskey, limit)
-        call.enqueue(object : Callback<Tickers?> {
-
-            override fun onResponse(
-                call: Call<Tickers?>,
-                response: Response<Tickers?>
-            )
-            {
-                if (response.isSuccessful()) {
-                    println("Success")
-                    println(response.body()?.data?.get(1)?.toString())
-                }
-            }
-
-            override fun onFailure(call: Call<Tickers?>, t: Throwable) {
-                println("Error with Tickers API")
-
-            }
-        }
-        )
-
-
-    }
-*/
-
 
 }
